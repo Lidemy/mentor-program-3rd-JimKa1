44c24273c84aa313834067c4a57c182d8b7402ac
Jim <wert723jim@gmail.com>
2019-06-24 04:27:47 +0800

    week3 hw

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,14 @@
 function isPalindromes(str) {
-  console.log(str);
+  let arr = str.split('');
+  let str2 = '';
+  for(let i = 0; i < arr.length; i ++){
+    str2 += arr[arr.length - i - 1];
+  }
+  
+  if(str == str2){
+    return true;
+  }else{
+    return false;
+  }
 }
-
 module.exports = isPalindromes;
